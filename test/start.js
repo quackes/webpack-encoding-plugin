3e4728e6ce968e00386da710b80b625e7c5e7feb
Ivan Rudoy <rudooy@gmail.com>
2019-02-27 23:33:44 +1000

    Update test

--- test/start.js
+++ test/start.js
@@ -2,22 +2,16 @@
  * Created by pd06093 on 12.11.2015.
  */
 
-var webpack = require("webpack");
-var path = require("path");
+const webpack = require('webpack');
+const path = require('path');
 
-var encodingPlugin = require('..');
+const EncodingPlugin = require('..');
 
 webpack({
-    context: __dirname,
-    entry : './src/test.js',
-    output : {
-        path : 'dist',
+    entry: './src/test.js',
+    output: {
+        path: path.resolve(__dirname, ''),
         filename: 'bundle.js'
     },
-    resolveLoader : {
-        root : path.join(__dirname, '../node_modules')
-
-    },
-
-    plugins : [  new encodingPlugin('iso-8859-1')]
-}, function(){});
+    plugins: [new EncodingPlugin('iso-8859-1')]
+});
